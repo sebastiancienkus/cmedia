c17f26b0a1ef21ea87b8672782fc133b4f99beae
Sebastian Cienkuss <sebastiancienkus@gmail.com>
2026-03-28 13:36:33 +0100

    zmiany w stylach

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    // Twój pełny adres URL - to pomaga Astro w generowaniu poprawnych ścieżek
-    site: 'https://ai.sebastiancienkus.com',
-    base: '/',
-    output: 'static',
-    build: {
-        // Generuje 'index.html' zamiast folderów, co Seohost obsługuje najlepiej
-        format: 'file'
-    }
+  // Twój pełny adres URL - to pomaga Astro w generowaniu poprawnych ścieżek
+  site: 'https://ai.sebastiancienkus.com',
+
+  base: '/',
+  output: 'static',
+
+  build: {
+      // Generuje 'index.html' zamiast folderów, co Seohost obsługuje najlepiej
+      format: 'file'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
