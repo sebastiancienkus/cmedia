6d76b08856460d83234dda2baea7223a33f9aa5e
sebastiancienkus <sebastiancienkus@gmail.com>
2026-03-28 14:00:48 +0100

    Change file path for content.json to public directory

--- public/cm-panel/api/save.php
+++ public/cm-panel/api/save.php
@@ -8,7 +8,7 @@ $moje_haslo = '727911300';
 $github_token = 'SEKRETNY_TOKEN_Z_GITHUBA'; 
 $github_user = 'sebastiancienkus';
 $github_repo = 'cmedia';
-$file_path = 'src/data/content.json';
+$file_path = 'public/content.json';  
 // --------------------
 
 // 1. Uniwersalne sprawdzenie hasła
